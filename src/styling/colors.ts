f82b8b29ca5072e29ad1161a2c9587fbd7691a34
kimbouvet <kim.oftedal@bouvet.no>
2019-02-18 11:25:35 +0100

    Implement theme

---
+++ src/styling/colors.ts
@@ -0,0 +1,12 @@
+import teal from "@material-ui/core/colors/teal";
+
+const Colors = {
+  primary: "#3FB8AF",
+  secondary: "#FF9E9D",
+  black: "black",
+  white: "#fff",
+  teal,
+  darkTeal: teal[700]
+};
+
+export default Colors;
