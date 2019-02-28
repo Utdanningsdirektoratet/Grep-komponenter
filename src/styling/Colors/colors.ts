703ab89822c05811f787ffd6d88fea4b2832e03b
Martin Harbo <harbo.martin@gmail.com>
2019-02-28 12:35:55 +0100

    export colors as strings

--- src/styling/Colors/colors.ts
+++ src/styling/Colors/colors.ts
@@ -1,5 +1,11 @@
 import teal from "@material-ui/core/colors/teal";
 import cyan from "@material-ui/core/colors/cyan";
+import blue from "@material-ui/core/colors/blue";
+import pink from "@material-ui/core/colors/pink";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import lime from "@material-ui/core/colors/lime";
+import amber from "@material-ui/core/colors/amber";
+import deepOrange from "@material-ui/core/colors/deepOrange";
 import red from "@material-ui/core/colors/red";
 import green from "@material-ui/core/colors/green";
 import yellow from "@material-ui/core/colors/yellow";
@@ -13,22 +19,28 @@ import blueGrey from "@material-ui/core/colors/blueGrey";
 import brown from "@material-ui/core/colors/brown";
 
 const Colors = {
-  cyan,
-  red,
-  green,
-  yellow,
-  grey,
-  orange,
-  lightBlue,
-  lightGreen,
-  purple,
-  indigo,
-  blueGrey,
-  brown,
+  red: red[500],
+  pink: pink[500],
+  purple: purple[500],
+  deepPurple: deepPurple[500],
+  indigo: indigo[500],
+  blue: blue[500],
+  lightBlue: lightBlue[500],
+  cyan: cyan[500],
+  teal: teal[500],
+  darkTeal: teal[700],
+  green: green[500],
+  lightGreen: lightGreen[500],
+  lime: lime[500],
+  yellow: yellow[500],
+  amber: amber[500],
+  orange: orange[500],
+  deepOrange: deepOrange[500],
+  brown: brown[500],
+  grey: grey[500],
+  blueGrey: blueGrey[500],
   black: "black",
-  white: "#fff",
-  teal,
-  darkTeal: teal[700]
+  white: "#fff"
 };
 
 export default Colors;
