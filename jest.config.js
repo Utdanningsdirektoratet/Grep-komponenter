4389f6ed5c4c687867759ed449506ad4bac3b99b
Martin Harbo <harbo.martin@gmail.com>
2019-03-17 23:22:08 +0100

    inital setup for running cra test against storybook/rollup setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  // Sett opp Enzyme
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupTestFrameworkScriptFile: "<rootDir>/src/setupEnzyme.ts"
+};
