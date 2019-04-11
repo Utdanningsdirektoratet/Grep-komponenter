ee561e9427214b8406cff70f6a63b0306aae9e08
Martin Harbo <harbo.martin@gmail.com>
2019-04-11 21:23:55 +0200

    enable elevation as prop to grey cover

--- src/components/GreyCover/GreyCover.tsx
+++ src/components/GreyCover/GreyCover.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import { StyledGreyCover } from "./greyCoverStyles";
 
-export interface GreyCoverProps {}
+export interface GreyCoverProps {
+    elevation?: number;
+}
 
 const GreyCover: React.FC<GreyCoverProps> = props => (
-    <StyledGreyCover>{props.children}</StyledGreyCover>
+    <StyledGreyCover elevation={props.elevation}>
+        {props.children}
+    </StyledGreyCover>
 );
 
 export default GreyCover as React.ComponentType<GreyCoverProps>;
