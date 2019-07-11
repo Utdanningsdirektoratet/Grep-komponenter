2131f00056ad4cdcabd57529bb755c46c9bc3446
Martin Harbo <harbo.martin@gmail.com>
2019-07-11 14:16:09 +0200

    implemented new design and hide on scroll

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,12 +1,36 @@
 import * as React from "react";
+import Toolbar from "@material-ui/core/Toolbar";
 import { StyledAppBar } from "./appBarStyles";
+import Slide from "@material-ui/core/Slide";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 export interface AppBarProps {}
 
+interface IHideOnScrollProps {
+    window?: () => Window;
+    children: React.ReactElement;
+}
+
+const HideOnScroll: React.FC<IHideOnScrollProps> = ({ children }) => {
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    // const trigger = useScrollTrigger({ target: window ? window() : undefined });
+    const trigger = useScrollTrigger();
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+};
+
 const AppBar: React.FC<AppBarProps> = props => (
-    <StyledAppBar color="primary" position="static" elevation={0}>
-        {props.children}
-    </StyledAppBar>
+    <HideOnScroll {...props}>
+        <StyledAppBar position="static" color="inherit" elevation={0}>
+            <Toolbar>{props.children}</Toolbar>
+        </StyledAppBar>
+    </HideOnScroll>
 );
 
 export default AppBar as React.ComponentType<AppBarProps>;
--- src/components/AppBar/appBarStyles.ts
+++ src/components/AppBar/appBarStyles.ts
@@ -2,5 +2,9 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from "@material-ui/core/AppBar";
 
 export const StyledAppBar = withStyles({
-    root: {}
+    root: {
+        backgroundColor: "transparant",
+        display: "flex",
+        flexDirection: "row"
+    }
 })(AppBar);
