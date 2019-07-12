947d914cc9658b6a3b37912a865e9b26cf497c5b
Martin Harbo <harbo.martin@gmail.com>
2019-07-12 13:22:25 +0200

    dependency cleanup

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,4 @@ export { default as InfoContainer } from "./components/InfoContainer";
 export { default as OverflowTooltip } from "./components/OverflowTooltip";
 export { default as Sidebar } from "./components/Sidebar";
 export { default as SidebarWrapper } from "./components/SidebarWrapper";
-
 export * from "./components";
-export * from "@material-ui/core";
