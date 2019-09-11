610bc309759a275c96401e4e4fd1a784d810f436
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2019-09-11 17:42:11 +0200

    Added support for custom table headers and updated typescript

--- src/components/GrepTable/GrepTable.tsx
+++ src/components/GrepTable/GrepTable.tsx
@@ -24,7 +24,7 @@ import {
 } from "@material-ui/core";
 
 export interface ITableColumn<T> {
-    label: string;
+    label: string | JSX.Element;
     width?: number;
     colDef?: string;
     sortable?: boolean;
