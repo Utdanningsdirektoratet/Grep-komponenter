94bb1636df73b4016129f72575b4154155ffa03f
odin thomas rochmann <odin.rochmann@gmail.com>
2019-11-12 14:14:53 +0100

    feat: add release config

--- release.sh
+++ release.sh
@@ -3,7 +3,10 @@
 BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 case $BRANCH in
     master)
-        npm version minor
+        npm version minor -m "Bumping to %s"
+    ;;
+    dev)
+        npm version patch -m "Bumping to %s"
     ;;
     *)
         npm version prerelease --preid=$BRANCH --no-git-tag-version
