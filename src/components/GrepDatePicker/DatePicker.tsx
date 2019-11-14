eef3c7ea789b1f9184501fbd79b8084acda908ac
odin thomas rochmann <odin.rochmann@gmail.com>
2019-11-14 15:54:35 +0100

    feat: move datepicker provider to component
    using datepicker from external library will fail because of context switching

--- src/components/GrepDatePicker/DatePicker.tsx
+++ src/components/GrepDatePicker/DatePicker.tsx
@@ -1,40 +1,53 @@
 import React, { useEffect } from 'react';
+import dayjs from 'dayjs';
+import 'dayjs/locale/nb';
+import DayjsIo from '@date-io/dayjs';
 import {
-    KeyboardDatePicker,
-    KeyboardDatePickerProps
+  KeyboardDatePicker,
+  KeyboardDatePickerProps,
+  MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
-import { useDate, DateInput } from '../../hooks';
+import { useDate } from '../../hooks';
+import { ParseableDate } from '../../utils/dateHelper';
+
+type DateInput = ParseableDate | null;
 
 export interface DatePickerProps
-    extends Omit<KeyboardDatePickerProps, 'value'> {
-    value?: DateInput;
-    errorMessage?: string;
+  extends Omit<KeyboardDatePickerProps, 'value'> {
+  value?: DateInput;
+  errorMessage?: string;
 }
 
-export default ({
-    value,
-    onChange,
-    errorMessage,
-    ...props
+dayjs.locale('nb');
+
+export const DatePicker: React.FunctionComponent<DatePickerProps> = ({
+  value,
+  onChange,
+  errorMessage,
+  ...props
 }: DatePickerProps) => {
-    const [date, setDate] = useDate(value);
-    useEffect(() => onChange(date), [date]);
-    const { error, ...overridden } = props;
-    const helperText = errorMessage || props.helperText;
-    return (
-        <KeyboardDatePicker
-            // default
-            clearable
-            format={'DD/MM/YYYY'}
-            invalidDateMessage={'Ugyldig dato'}
-            margin="normal"
-            // logic
-            {...overridden}
-            {...(error && error)}
-            {...(helperText && helperText)}
-            // helperText={errorMessage || props.helperText}
-            value={date}
-            onChange={setDate}
-        />
-    );
+  const [date, setDate] = useDate(value);
+  useEffect(() => onChange(date), [date]);
+  const { error, ...overridden } = props;
+  const helperText = errorMessage || props.helperText;
+  return (
+    <MuiPickersUtilsProvider utils={DayjsIo} locale={'nb'}>
+      <KeyboardDatePicker
+        // default
+        clearable
+        format={'DD/MM/YYYY'}
+        invalidDateMessage={'Ugyldig dato'}
+        margin="normal"
+        // logic
+        {...overridden}
+        {...(error && error)}
+        {...(helperText && helperText)}
+        // helperText={errorMessage || props.helperText}
+        value={date}
+        onChange={setDate}
+      />
+    </MuiPickersUtilsProvider>
+  );
 };
+
+export default DatePicker;
--- src/components/GrepDateRange/index.tsx
+++ src/components/GrepDateRange/index.tsx
@@ -2,52 +2,63 @@ import React, { useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 
 import DatePicker, { DatePickerProps } from '../GrepDatePicker';
-import { useDate, DateState } from '../../hooks';
+import { useDate, DateState } from '../../hooks/use-date';
 
 import { GridSpacing } from '@material-ui/core/Grid';
 
 interface Props extends Pick<DatePickerProps, 'inputVariant'> {
-    from: Omit<DatePickerProps, 'onChange'>;
-    to: Omit<DatePickerProps, 'onChange'>;
-    onChange: (date: { from: DateState; to: DateState }) => void;
-    // container
-    spacing?: GridSpacing;
+  from: Omit<DatePickerProps, 'onChange'>;
+  to: Omit<DatePickerProps, 'onChange'>;
+  onChange: (date: { from: DateState; to: DateState }) => void;
+  // container
+  spacing?: GridSpacing;
+  disabled?: boolean;
 }
 
-export default ({ onChange, spacing, inputVariant, ...props }: Props) => {
-    const [from, setFrom] = useDate(props.from.value);
-    const [to, setTo] = useDate(props.to.value);
-    useEffect(() => onChange({ from, to }), [from, to]);
-    return (
-        <Grid container spacing={spacing || 3}>
-            <Grid item xs={12} sm={6}>
-                <DatePicker
-                    // default
-                    fullWidth
-                    label={props.from.label}
-                    inputVariant={inputVariant}
-                    maxDateMessage={`Dato må være først "${props.to.label}"`}
-                    // logic
-                    {...props.from }
-                    value={from}
-                    maxDate={to || undefined}
-                    onChange={setFrom}
-                />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <DatePicker
-                    // default
-                    fullWidth
-                    label={props.to.label}
-                    inputVariant={inputVariant}
-                    minDateMessage={`Dato må være først "${props.from.label}"`}
-                    // logic
-                    {...props.to }
-                    value={to}
-                    minDate={from || undefined}
-                    onChange={setTo}
-                />
-            </Grid>
-        </Grid>
-    );
+export const GrepDateRange: React.FunctionComponent<Props> = ({
+  onChange,
+  spacing,
+  inputVariant,
+  disabled,
+  ...props
+}: Props) => {
+  const [from, setFrom] = useDate(props.from.value);
+  const [to, setTo] = useDate(props.to.value);
+  useEffect(() => onChange({ from, to }), [from, to]);
+  return (
+    <Grid container spacing={spacing || 3}>
+      <Grid item xs={12} sm={6}>
+        <DatePicker
+          // default
+          fullWidth
+          disabled={disabled}
+          label={props.from.label}
+          inputVariant={inputVariant}
+          maxDateMessage={`Dato må være først "${props.to.label}"`}
+          // logic
+          {...props.from}
+          value={from}
+          maxDate={to || undefined}
+          onChange={setFrom}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <DatePicker
+          // default
+          fullWidth
+          disabled={disabled}
+          label={props.to.label}
+          inputVariant={inputVariant}
+          minDateMessage={`Dato må være først "${props.from.label}"`}
+          // logic
+          {...props.to}
+          value={to}
+          minDate={from || undefined}
+          onChange={setTo}
+        />
+      </Grid>
+    </Grid>
+  );
 };
+
+export default GrepDateRange;
