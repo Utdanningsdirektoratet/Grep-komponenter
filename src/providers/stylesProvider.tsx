6596fbeee5833938c1bb359a8bebc682b00cc00e
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2020-11-20 14:00:21 +0100

    chore: cleanup and minor fixes - removed @material-ui/styles (included in @material-ui/core) - add react-redux provider to all stories

--- src/providers/stylesProvider.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { StylesProvider as MuiStylesProvider } from '@material-ui/styles';
-
-const StylesProvider: React.FC<{}> = ({ children }) => {
-  return <MuiStylesProvider injectFirst>{children}</MuiStylesProvider>;
-};
-
-export default StylesProvider;
