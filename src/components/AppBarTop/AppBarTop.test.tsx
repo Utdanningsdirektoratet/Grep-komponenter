f1e168867f830d22beadf1c2cb476444cdf90240
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2021-09-28 14:56:41 +0200

    test(grepf-1456): add tests for AppBarTop

--- src/components/AppBarTop/AppBarTop.test.tsx
+++ src/components/AppBarTop/AppBarTop.test.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import AppBarTop from './AppBarTop';
 
 describe('AppBarProfile', () => {
   it('should render correctly', () => {
-    render(<AppBarTop />);
+    render(
+      <div data-testid="container">
+        <AppBarTop>
+          <p>Child #1</p>
+          <p>Child #2</p>
+          <p>Child #3</p>
+        </AppBarTop>
+      </div>,
+    );
+
+    const appBarTop = screen.getByTestId('container').firstChild;
+
+    expect(appBarTop).toBeVisible();
+
+    expect(appBarTop?.childNodes.length).toBe(3);
   });
 });
