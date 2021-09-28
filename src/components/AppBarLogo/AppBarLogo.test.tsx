eb490b479a64846cb75506a030f404e63c7afb05
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2021-09-28 14:48:55 +0200

    test(grepf-1456): add tests for AppBarLogo

--- src/components/AppBarLogo/AppBarLogo.test.tsx
+++ src/components/AppBarLogo/AppBarLogo.test.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import AppBarLogo from './AppBarLogo';
 
 describe('AppBarLogo', () => {
+  beforeEach(() => {
+    render(
+      <AppBarLogo
+        title="TitleTest"
+        environment="EnvironmentTest"
+        imageSrc="./logo.png"
+        imageAlt={'logo.png'}
+      />,
+    );
+  });
+
   it('should render correctly', () => {
-    render(<AppBarLogo title="" imageAlt="" environment="" imageSrc="" />);
+    expect(screen.getByText('TitleTest')).toBeVisible();
+    expect(screen.getByText('EnvironmentTest')).toBeVisible();
+    expect(screen.getByRole('img')).toBeVisible();
+    expect(screen.getByAltText('logo.png')).toBeVisible();
   });
 });
