0eac52326a62c94dba1bf06efad7f455fa12e33a
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2021-10-05 09:29:22 +0200

    test(grepf-1456): improve ConfirmDialog tests

--- src/components/ConfirmationDialog/ConfirmationDialog.test.tsx
+++ src/components/ConfirmationDialog/ConfirmationDialog.test.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { Button } from '@material-ui/core';
-import { render, screen, waitFor } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { ConfirmationServiceProvider, useConfirmation } from '.';
@@ -32,74 +36,54 @@ const renderComponent = (warning?: boolean) =>
 
 describe('ConfirmationDialog', () => {
   it('should render correctly', async () => {
-    const { container } = renderComponent();
+    const { getByRole, getByText } = renderComponent();
 
     // Dialog should not render by default
     expect(screen.queryByRole('presentation')).toBeFalsy();
 
-    // Open dialog
-    userEvent.click(container.querySelector('button')!);
+    userEvent.click(getByRole('button', { name: /open dialog/i }));
 
     // Dialog should render when opened
-    expect(screen.queryByRole('presentation')).toBeVisible();
+    expect(getByRole('presentation')).toBeVisible();
 
     // Dialog should contain title and description text
-    expect(screen.getByText('Title text')).toBeVisible();
-    expect(screen.getByText('Description text')).toBeVisible();
-
-    const confirmBtn = screen.getByText('Confirm').parentElement;
-    const cancelBtn = screen.getByText('Cancel').parentElement;
+    expect(getByText('Title text')).toBeVisible();
+    expect(getByText('Description text')).toBeVisible();
 
     // Dialog buttons should be visible
-    expect(confirmBtn).toBeVisible();
-    expect(cancelBtn).toBeVisible();
-
-    // Dialog buttons should be of type 'button'
-    expect(confirmBtn?.getAttribute('type')).toBe('button');
-    expect(cancelBtn?.getAttribute('type')).toBe('button');
+    expect(getByRole('button', { name: /confirm/i })).toBeVisible();
+    expect(getByRole('button', { name: /cancel/i })).toBeVisible();
   });
 
   it('should handle confirm', async () => {
-    const { container } = renderComponent();
+    const { getByRole } = renderComponent();
 
-    // Open dialog
-    userEvent.click(container.querySelector('button')!);
+    userEvent.click(getByRole('button', { name: /open dialog/i }));
 
-    // Click 'confirm'-button
-    userEvent.click(screen.getByText('Confirm').parentElement!);
+    userEvent.click(getByRole('button', { name: /confirm/i }));
 
-    await waitFor(() => {
-      expect(mockFn).toHaveBeenCalledWith('confirmed');
+    await waitForElementToBeRemoved(() => getByRole('presentation'));
 
-      // Dialog should be closed
-      expect(screen.queryByRole('presentation')).toBeFalsy();
-    });
+    expect(mockFn).toHaveBeenCalledWith('confirmed');
   });
 
   it('should handle cancel', async () => {
-    const { container } = renderComponent();
+    const { getByRole } = renderComponent();
 
-    // Open dialog
-    userEvent.click(container.querySelector('button')!);
+    userEvent.click(getByRole('button', { name: /open dialog/i }));
 
-    // Click 'cancel'-button
-    userEvent.click(screen.getByText('Cancel').parentElement!);
+    userEvent.click(getByRole('button', { name: /cancel/i }));
 
-    await waitFor(() => {
-      // Dialog should be closed
-      expect(screen.queryByRole('presentation')).toBeFalsy();
-    });
+    await waitForElementToBeRemoved(() => getByRole('presentation'));
   });
 
   it("should have correct className when 'warning'-prop is true", () => {
-    const { container } = renderComponent(true);
+    const { getByRole } = renderComponent(true);
 
-    // Open dialog
-    userEvent.click(container.querySelector('button')!);
+    userEvent.click(getByRole('button', { name: /open dialog/i }));
 
-    const confirmBtn = screen.getByText('Confirm').parentElement;
+    const confirmBtn = getByRole('button', { name: /confirm/i });
 
-    // Should include 'discard'-className when 'warning'-prop is true
-    expect(confirmBtn?.className.includes('makeStyles-discard')).toBe(true);
+    expect(confirmBtn.className).toContain('makeStyles-discard');
   });
 });
