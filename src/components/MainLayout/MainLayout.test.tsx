75ef1a60be43a350c9e6c7296139a1fec196326a
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2021-10-11 11:14:59 +0200

    test(grepf-1456): add tests for MainLayout

--- src/components/MainLayout/MainLayout.test.tsx
+++ src/components/MainLayout/MainLayout.test.tsx
@@ -5,10 +5,21 @@ import MainLayout from './MainLayout';
 
 describe('MainLayout', () => {
   it('should render correctly', () => {
-    render(
-      <MainLayout>
-        <p>test</p>
-      </MainLayout>,
+    const { getByTestId, getByText } = render(
+      <div data-testid="container">
+        <MainLayout>
+          <p>test</p>
+        </MainLayout>
+      </div>,
     );
+
+    expect(getByText(/test/i)).toBeVisible();
+
+    const layout = getByTestId('container').firstChild;
+    expect(layout).toHaveStyle({
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+    });
   });
 });
