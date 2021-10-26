34175a738c1814f240916a7996554ed6ced90adf
Aleksander Vevle <aleksander.vevle@bouvet.no>
2021-10-26 10:56:14 +0200

    feat: added responsivness to BodyLayout

--- src/components/BodyLayout/bodyLayoutStyles.ts
+++ src/components/BodyLayout/bodyLayoutStyles.ts
@@ -1,11 +1,21 @@
 import { Box, withStyles } from '@material-ui/core';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { createStyles } from '@material-ui/styles';
 
-export const StyledBodyLayout = withStyles({
-  root: {
-    width: '100%',
-    paddingTop: 20,
-    display: 'flex',
-    marginBottom: 20,
-    justifyContent: 'space-between',
-  },
-})(Box);
+export const StyledBodyLayout = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      paddingTop: 20,
+      display: 'flex',
+      marginBottom: 20,
+      justifyContent: 'space-between',
+      [theme.breakpoints.down('md')]: {
+        flexDirection: 'column',
+      },
+      [theme.breakpoints.up('md')]: {
+        flexDirection: 'row',
+      },
+    },
+  }),
+)(Box);
