0d6d8a896e23249842d5956158223004a8218da3
Aleksander Vevle <aleksander.vevle@bouvet.no>
2021-10-28 07:28:28 +0200

    fix(grepf-1498):  revert changes to BodyLayout

--- src/components/BodyLayout/bodyLayoutStyles.ts
+++ src/components/BodyLayout/bodyLayoutStyles.ts
@@ -1,21 +1,15 @@
 import { Box, withStyles } from '@material-ui/core';
-import { Theme } from '@material-ui/core';
 import { createStyles } from '@material-ui/styles';
 
-export const StyledBodyLayout = withStyles((theme: Theme) =>
+export const StyledBodyLayout = withStyles(() =>
   createStyles({
     root: {
       width: '100%',
       paddingTop: 20,
       display: 'flex',
+      flexDirection: 'row',
       marginBottom: 20,
       justifyContent: 'space-between',
-      [theme.breakpoints.down('md')]: {
-        flexDirection: 'column',
-      },
-      [theme.breakpoints.up('md')]: {
-        flexDirection: 'row',
-      },
     },
   }),
 )(Box);
