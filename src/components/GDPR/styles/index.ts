099aa58e82afb73aa0ccd239578c26ff193071d7
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2021-11-16 12:00:14 +0100

    fix(grepf-1506): fix invalid DOM nesting

--- src/components/GDPR/styles/index.ts
+++ src/components/GDPR/styles/index.ts
@@ -1,43 +1,23 @@
-import Info from '@material-ui/icons/Info';
-import { Box, Typography, withStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
-export const Container = withStyles({
-  root: {
+export const useStyles = makeStyles({
+  container: {
     backgroundColor: 'rgb(241, 243, 244)',
     height: 'fit-content',
     display: 'flex',
     maxWidth: 500,
   },
-})(Box);
-
-export const Content = withStyles({
-  root: {
+  content: {
     backgroundColor: 'unset',
     marginRight: 20,
   },
-})(Box);
-
-export const Title = withStyles({
-  root: {
+  icon: {
+    margin: '20px 10px',
+    color: 'rgb(255, 158, 157)',
+  },
+  title: {
     backgroundColor: 'unset',
     fontSize: 16,
     margin: '20px 0',
   },
-})(Typography);
-
-export const Body = withStyles({
-  root: {
-    backgroundColor: 'unset',
-
-    h4: {
-      marginRight: 20,
-    },
-  },
-})(Typography);
-
-export const StyledIcon = withStyles({
-  root: {
-    margin: '20px 10px',
-    color: 'rgb(255, 158, 157)',
-  },
-})(Info);
+});
