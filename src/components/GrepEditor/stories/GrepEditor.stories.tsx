280fe4a667582b2980fce955365136dbb2fb082a
Oddvar-Nordbø-Øksendal <oddvar.oksendal@bouvet.no>
2024-04-30 13:17:07 +0200

    feat(GREPF-1810): update editorstate outside of lexicalcc, plugin to prevent unwanted styling

--- src/components/GrepEditor/stories/GrepEditor.stories.tsx
+++ src/components/GrepEditor/stories/GrepEditor.stories.tsx
@@ -7,7 +7,6 @@ import { ToolbarProperties } from '../entities';
 import { $getTextContent } from 'lexical';
 import { $generateHtmlFromNodes } from '@lexical/html';
 import LexicalButton from '../components/buttons/InlineButton';
-
 const useStyles = makeStyles()({
   root: {
     background: 'red',
